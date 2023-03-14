9a9f67326463aa4e0b70fd1848721ee15ac9ead8
Eyüp Mert <eupmrt@gmail.com>
2023-03-14 21:04:34 +0300

    Improve: Premium Screen 2

--- Coffee Dictionary/Globals/GlobalFunctions.swift
+++ Coffee Dictionary/Globals/GlobalFunctions.swift
@@ -29,22 +29,30 @@ extension UIView {
         
     }
     
-    func addBottomRoundedEdge() {
-          let offset: CGFloat = (self.frame.width * 1.5)
-          let bounds: CGRect = self.bounds
-
-          let rectBounds: CGRect = CGRect(x: bounds.origin.x, y: bounds.origin.y, width: bounds.size.width , height: bounds.size.height / 2)
-          let rectPath: UIBezierPath = UIBezierPath(rect: rectBounds)
-          let ovalBounds: CGRect = CGRect(x: bounds.origin.x - offset / 2, y: bounds.origin.y, width: bounds.size.width + offset , height: bounds.size.height)
-          let ovalPath: UIBezierPath = UIBezierPath(ovalIn: ovalBounds)
-          rectPath.append(ovalPath)
-
-          let maskLayer: CAShapeLayer = CAShapeLayer()
-          maskLayer.frame = bounds
-          maskLayer.path = rectPath.cgPath
-
-          self.layer.mask = maskLayer
-      }
+    func addBottomRoundedEdge(desiredCurve: CGFloat?, view: UIView) {
+            let offset: CGFloat = self.frame.width / desiredCurve!
+            let bounds: CGRect = self.bounds
+            
+            let rectBounds: CGRect = CGRect(x: bounds.origin.x, y: bounds.origin.y, width: bounds.size.width, height: bounds.size.height / 2)
+            let rectPath: UIBezierPath = UIBezierPath(rect: rectBounds)
+            let ovalBounds: CGRect = CGRect(x: bounds.origin.x - offset / 2, y: bounds.origin.y, width: bounds.size.width + offset, height: bounds.size.height)
+            let ovalPath: UIBezierPath = UIBezierPath(ovalIn: ovalBounds)
+            rectPath.append(ovalPath)
+            
+            // Create the shape layer and set its path
+            let maskLayer: CAShapeLayer = CAShapeLayer()
+            maskLayer.frame = bounds
+            maskLayer.path = rectPath.cgPath
+        
+       
+            
+            // Set the newly created shape layer as the mask for the view's layer
+            self.layer.mask = maskLayer
+        }
+    
+ 
+    
+    
  
 }
 
--- Coffee Dictionary/PremiumViewControllers/SecondPremiumVC/SecondPremiumVcCell.swift
+++ Coffee Dictionary/PremiumViewControllers/SecondPremiumVC/SecondPremiumVcCell.swift
@@ -10,16 +10,37 @@ import UIKit
 class SecondPremiumVcCell: UITableViewCell {
     
     
+    
+    
+    @IBOutlet weak var productView1: UIView!
+    @IBOutlet weak var productView2: UIView!
     @IBOutlet weak var topViewBg: UIView!
+    
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
 
-
+        productView1.addProductBorder()
+        
+        
+        if UIDevice.current.userInterfaceIdiom != .pad {
+            topViewBg.addBottomRoundedEdge(desiredCurve: 1, view: topViewBg)
+          //  topViewBg.makeRoundedAndShadowed(view: topViewBg)
+      
+        }
+        
+        
 
 
     }
-
+    
+    
+    @IBAction func dismissPressed(_ sender: Any) {
+    }
+    
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
